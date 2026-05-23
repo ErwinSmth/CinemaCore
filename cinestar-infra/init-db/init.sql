7e6de3ad450c7b6ffc15cce34dcb2025fcd75553
David <jesusd9perez@gmail.com>
2026-05-23 00:31:10 -0500

    Inicializacion del Proyecto

---
+++ cinestar-infra/init-db/init.sql
@@ -0,0 +1,5 @@
+-- Creación automática de las bases de datos lógicas para cada microservicio
+CREATE DATABASE db_auth;
+CREATE DATABASE db_movies;
+CREATE DATABASE db_showtime;
+CREATE DATABASE db_seats;
