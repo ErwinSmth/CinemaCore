3abc168aa221f1ba503d0197c5043881da7b4178
ErwinSmth <jesusd9perez@gmail.com>
2026-06-29 11:40:29 -0500

    correcciones

---
+++ backend/movie-service/src/main/java/pe/edu/utp/cinestar/movie_service/service/CacheWarmingService.java
@@ -0,0 +1,18 @@
+package pe.edu.utp.cinestar.movie_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CacheWarmingService {
+
+    private final MovieService movieService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void warmUpCache() {
+        movieService.getCartelera(null, null);
+    }
+}
